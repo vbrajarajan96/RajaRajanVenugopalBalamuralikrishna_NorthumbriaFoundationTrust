781ce33b35e2a8e1bf2d4220bc6e42250e6922ce
Raja Rajan <vbrajarajan96@gmail.com>
2025-08-19 12:23:10 +0100

    Update

--- NorthumbriaFoundationTrust.Tests/Steps/SearchSteps.cs
+++ NorthumbriaFoundationTrust.Tests/Steps/SearchSteps.cs
@@ -1,10 +1,10 @@
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using TechTalk.SpecFlow;
 using Microsoft.Playwright;
 using NorthumbriaFoundationTrust.Tests.Pages;
 using NorthumbriaFoundationTrust.Tests.Support;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
 
 namespace NorthumbriaFoundationTrust.Tests.Steps
 {
@@ -13,6 +13,9 @@ namespace NorthumbriaFoundationTrust.Tests.Steps
     {
         private readonly UiContext _ctx;
         private HomePage? _home;
+        private SearchResultsPage? _results;
+        private QualitySafetyPage? _quality;
+        private string? _searchTerm;
 
         public SearchSteps(UiContext ctx) => _ctx = ctx;
 
@@ -28,6 +31,7 @@ namespace NorthumbriaFoundationTrust.Tests.Steps
         [When(@"the user enters ""(.*)"" in the search field")]
         public async Task WhenTheUserEntersInTheSearchField(string term)
         {
+            _searchTerm = term;
             await _home!.EnterSearchAsync(term);
         }
 
@@ -35,21 +39,22 @@ namespace NorthumbriaFoundationTrust.Tests.Steps
         public async Task WhenTheUserClicksTheSearchButton()
         {
             await _home!.SubmitSearchByButtonAsync();
-            await _ctx.Page.Locator("h2.results-heading").GetByText("Page results").WaitForAsync();
-            await _ctx.Page.Locator("#search-results").WaitForAsync();
+            _results = new SearchResultsPage(_ctx.Page);
+            await _results.WaitForResultsAsync(_searchTerm ?? UiContext.Texts.SearchTerm);
         }
 
         [When(@"the user presses Enter to submit the search")]
         public async Task WhenTheUserPressesEnterToSubmitTheSearch()
         {
             await _home!.SubmitSearchByEnterAsync();
-            await _ctx.Page.Locator("h2.results-heading").GetByText("Page results").WaitForAsync();
-            await _ctx.Page.Locator("#search-results").WaitForAsync();
+            _results = new SearchResultsPage(_ctx.Page);
+            await _results.WaitForResultsAsync(_searchTerm ?? UiContext.Texts.SearchTerm);
         }
 
         [Then(@"results relevant to ""(.*)"" are displayed")]
         public async Task ThenResultsRelevantToAreDisplayed(string term)
         {
+            _results ??= new SearchResultsPage(_ctx.Page);
             var hit = _ctx.Page.Locator("#search-results a", new() { HasText = term }).First;
             await hit.WaitForAsync();
             Assert.That(await hit.IsVisibleAsync(), Is.True, $"Expected at least one result containing '{term}'.");
@@ -58,20 +63,15 @@ namespace NorthumbriaFoundationTrust.Tests.Steps
         [Then(@"the user selects the ""(.*)"" link on the results page")]
         public async Task ThenTheUserSelectsTheLinkOnTheResultsPage(string linkText)
         {
-            var results = _ctx.Page.Locator("#search-results");
-            var target = results.Locator("ul.results li.search-result a", new() { HasText = linkText }).First;
-            await target.ClickAsync();
+            _results ??= new SearchResultsPage(_ctx.Page);
+            await _results.OpenResultAsync(linkText);
         }
 
         [Then(@"the user navigates to the ""(.*)"" page")]
         public async Task ThenTheUserNavigatesToThePage(string linkText)
         {
-            var qsLink = _ctx.Page.GetByRole(AriaRole.Link, new() { Name = linkText });
-            var fallback = _ctx.Page.Locator("a:has-text('" + linkText + "')").First;
-            if (await qsLink.CountAsync() > 0)
-                await qsLink.ClickAsync();
-            else
-                await fallback.ClickAsync();
+            _quality ??= new QualitySafetyPage(_ctx.Page);
+            await _quality.OpenTileAsync(linkText);
             await _ctx.Page.WaitForURLAsync(new Regex(".*/continually-improving-services/?$", RegexOptions.IgnoreCase));
         }
 
